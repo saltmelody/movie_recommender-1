d9c93f12426d62231247d9e0a9b980073efc9fe1
masa3141 <masahiro@kazama.tv>
2023-09-01 17:57:14 +0900

    add data

--- app.py
+++ app.py
@@ -6,23 +6,19 @@ import gensim
 st.title('映画レコメンド')
 
 # 映画評価データの読み込み
-movielens = pd.read_csv("data/movielens.tsv", sep="\t")
+movies = pd.read_csv("data/movies.tsv", sep="\t")
 
 # 学習済みのitem2vecモデルの読み込み
 model = gensim.models.word2vec.Word2Vec.load("data/item2vec.model")
 
-# 映画評価データの確認
-st.write("映画評価データ(確認用)")
-st.write(movielens.head(5))
-
 # 映画IDとタイトルを辞書型に変換
-movies = movielens["title"].unique()
-movie_ids = movielens["movie_id"].unique()
-movie_id_to_title = dict(zip(movie_ids, movies))
-movie_title_to_id = dict(zip(movies, movie_ids))
+movie_titles = movies["title"].tolist()
+movie_ids = movies["movie_id"].tolist()
+movie_id_to_title = dict(zip(movie_ids, movie_titles))
+movie_title_to_id = dict(zip(movie_titles, movie_ids))
 
 st.markdown("## 1本の映画に対して似ている映画を表示する")
-selected_movie = st.selectbox("映画を選んでください", movies)
+selected_movie = st.selectbox("映画を選んでください", movie_titles)
 selected_movie_id = movie_title_to_id[selected_movie]
 st.write(f"あなたが選択した映画は{selected_movie}(id={selected_movie_id})です")
 
@@ -38,7 +34,7 @@ st.write(results)
 
 st.markdown("## 複数の映画を選んでおすすめの映画を表示する")
 
-selected_movies = st.multiselect("映画を複数選んでください", movies)
+selected_movies = st.multiselect("映画を複数選んでください", movie_titles)
 selected_movie_ids = [movie_title_to_id[movie] for movie in selected_movies]
 vectors = [model.wv.get_vector(movie_id) for movie_id in selected_movie_ids]
 if len(selected_movies) > 0:
